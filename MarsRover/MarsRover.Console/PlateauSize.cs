9e0b2456467afdcd51b679e1c2869cc33bfda9ad
ibby <261290261+ibby1995@users.noreply.github.com>
2026-04-13 12:10:41 +0100

    Add position and PlateauSize data classes

---
+++ MarsRover/MarsRover.Console/PlateauSize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Console
+{
+    public class PlateauSize
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public PlateauSize(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ MarsRover/MarsRover.Console/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Console
+{
+    public class Position
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public CompassDirection Facing { get; set; }
+        public Position(int x, int y, CompassDirection facing)
+        {
+            this.x = x;
+            this.y = y;
+            Facing = facing;
+        }
+    }
+}
