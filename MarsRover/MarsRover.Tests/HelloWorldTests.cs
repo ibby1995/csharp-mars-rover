096ac882dfa88de36a6414419cad6442bfed0f63
ibby <261290261+ibby1995@users.noreply.github.com>
2026-04-13 11:20:31 +0100

    MarsRover Testing

---
+++ MarsRover/MarsRover.Tests/HelloWorldTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace MarsRover.Tests;
+
+public class HelloWorldTests
+{
+    [Test]
+    public void Successfully_runs_a_test()
+    {
+        Assert.That(true, Is.True);
+    }
+}
+
+
